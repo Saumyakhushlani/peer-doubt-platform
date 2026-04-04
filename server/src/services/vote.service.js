b9a92cd7d5b53c0a4bc7e6a8a4990625127a8d9b
Krishna Gupta <kg886120@gmail.com>
2026-04-05 03:51:06 +0530

    Added upvote in backend

--- server/src/services/vote.service.js
+++ server/src/services/vote.service.js
@@ -1,15 +1,46 @@
 import prisma from "../config/prisma.js";
 
 export const createVote = async (data) => {
+    if (data.questionId) {
+        await prisma.vote.deleteMany({ where: { userId: data.authorId, questionId: data.questionId } });
+    } else if (data.answerId) {
+        await prisma.vote.deleteMany({ where: { userId: data.authorId, answerId: data.answerId } });
+    }
+
     const vote = await prisma.vote.create({
         data: {
             type: data.type,
-            authorId: data.authorId,
+            userId: data.authorId,
             questionId: data.questionId,
             answerId: data.answerId,
         }
     });
     return vote;
+};
+
+export const deleteVote = async (data) => {
+    if (data.questionId) {
+        return await prisma.vote.deleteMany({
+            where: {    
+                userId: data.userId,
+                questionId: data.questionId,
+            }
+        });
+    }
+    if (data.answerId) {
+        return await prisma.vote.deleteMany({
+            where: {    
+                userId: data.userId,
+                answerId: data.answerId,
+            }
+        });
+    }
+};
+
+export const getMyVotes = async (userId) => {
+    return await prisma.vote.findMany({
+        where: { userId }
+    });
 };
 
 export const getVotesByQuestion = async (questionId) => {
@@ -31,9 +62,8 @@ export const getVotesByAnswer = async (answerId) => {
 };
 
 export const getVotesCountByQuestion = async (questionId) => {
-    const votes = await prisma.vote.count({
+    const votes = await prisma.vote.findMany({
         where: {
-
             questionId: questionId,
         }
     });
@@ -41,7 +71,7 @@ export const getVotesCountByQuestion = async (questionId) => {
 };
 
 export const getVotesCountByAnswer = async (answerId) => {
-    const votes = await prisma.vote.count({
+    const votes = await prisma.vote.findMany({
         where: {
             answerId: answerId,
         }
